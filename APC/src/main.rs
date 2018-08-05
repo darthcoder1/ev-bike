83fcd2971efc3fccefd2402729a973b3ac1d3f7e
Martin Zielinski <darthcoder1@gmail.com>
2018-08-05 15:42:57 +0200

    some experimentation with ITM

--- APC/src/main.rs
+++ APC/src/main.rs
@@ -1,19 +1,36 @@
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-extern crate aux6;
 #[macro_use]
 extern crate cortex_m;
 #[macro_use]
-extern crate cortex_m_rt;
+extern crate cortex_m_rt as rt;
+extern crate cortex_m_semihosting;
+extern crate panic_semihosting;
+
+use cortex_m::{asm, Peripherals};
+use rt::ExceptionFrame;
+use core::ptr;
 
 entry!(main);
 
+
 fn main() -> ! {
-    let mut itm = aux6::init();
 
-    iprintln!(&mut itm.stim[0], "Hello, world!");
+     loop {}
+ }
 
-    loop {}
-}
+ // define the hard fault handler
+ exception!(HardFault, hard_fault);
+
+ fn hard_fault(ef: &ExceptionFrame) -> ! {
+     panic!("HardFault at {:#?}", ef);
+ }
+
+ // define the default exception handler
+ exception!(*, default_handler);
+
+ fn default_handler(irqn: i16) {
+     panic!("Unhandled exception (IRQn = {})", irqn);
+ }
