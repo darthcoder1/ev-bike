3b3039d872c181ff59cb53db55f19f76827b1f0c
Martin Zielinski <darthcoder1@gmail.com>
2018-09-07 00:10:30 +0200

    Added deploy functionality
    The deploy script relies on st-flash util being installed. As WSL does not
    support st-link v2 natively, the windows executable of st-flash is executed..

--- APC/deploy.sh
+++ APC/deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+echo "Deploying to STM32"
 
-echo "Deploy to STM32 not supported yet"
+STLINK_BIN_DIR=~/Development/Tools/stlink-1.3.0-win64/bin
+FLASH_TOOL=$STLINK_BIN_DIR/st-flash.exe
+OUTPUT_BIN=target/thumbv7em-none-eabihf/debug/APC
+
+OUTPUT_HEX=$OUTPUT_BIN.hex
+
+arm-none-eabi-objcopy -O ihex $OUTPUT_BIN $OUTPUT_BIN.hex
+$FLASH_TOOL --format ihex write target/thumbv7em-none-eabihf/debug/APC.hex
+
+
+# OPENOCD setup
+# OPENOCD_DIR=~/Development/Tools/openocd_win
+# $OPENOCD_DIR/bin/openocd.exe -s $OPENOCD_DIR/share/scripts -f interface/stlink-v2.cfg -f target/stm32f1x.cfg
