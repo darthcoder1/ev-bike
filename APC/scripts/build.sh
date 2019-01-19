8935460584f40ffc558d6b6f6c1e9a3d5d365ed1
Martin Zielinski <martinz@unity3d.com>
2019-01-19 17:01:19 +0100

    Delete APC, moved into separate repository https://github.com/darthcoder1/evproj-APC

--- APC/scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Building firmware for STM32"
-
-# build the project
-~/.cargo/bin/cargo build --target=thumbv7em-none-eabihf
-
-echo "done" 
--- APC/scripts/connect_openocd.bat
+++
@@ -1,2 +0,0 @@
-
-openocd.exe -s D:\Development\Tools\openocd_win\share\scripts -f interface/stlink-v2.cfg -f target/stm32f1x.cfg
